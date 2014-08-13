18e84a6de65d5731ad2b548c6a48a1f49d1a92c5
awdavies <a.w.davies.vio@gmail.com>
2014-08-12 22:41:05 -0700

    Second attempt at getting things right.

--- awdavies/settings.py
+++ awdavies/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dzs7hct20k#k)&@fz+g5!vcb$!a51d-p4$3ta_nfu1+nakzb42'
+SECRET_KEY = 'm=l_0=6nksezez&xh7^us_i@_(*g%=dnug7c0%6jzpq84hf#lf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    #'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -50,6 +50,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'awdavies.urls'
 
 WSGI_APPLICATION = 'awdavies.wsgi.application'
+
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
@@ -73,6 +74,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
