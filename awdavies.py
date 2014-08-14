15ffaae793c33a05b548c5974cd51b59a37beaa8
Andrew Davies <awdavies@awdavies-macbookpro.roam.corp.google.com>
2014-08-14 10:07:05 -0700

    Removed dem pesky tabs.

--- awdavies.py
+++ awdavies.py
@@ -3,7 +3,7 @@ site = Flask(__name__)
 
 @site.route("/")
 def main():
-	return "This is the website of Andrew Davies!"
+    return "This is the website of Andrew Davies!"
 
 if __name__ == "__main__":
-	site.run()
+    site.run()
