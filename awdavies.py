e98fb0d31704387c612f2e2245adff0e4d45ca9d
awdavies <a.w.davies.vio@gmail.com>
2014-09-25 20:47:48 -0700

    Added in the main files and such.

--- awdavies.py
+++ awdavies.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import (
+Flask,
+render_template,
+url_for,
+)
 site = Flask(__name__)
+url_for('static', filename='narrow.css')
+url_for('static', filename='padding.css')
 
 @site.route("/")
 def main():
-    return "This is the website of Andrew Davies!"
+	return render_template("index.html")
 
 if __name__ == "__main__":
     site.run()
