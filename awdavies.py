42f5a4f947e77190b6c23183e3a3abb42920c7bd
awdavies <a.w.davies.vio@gmail.com>
2017-02-14 15:44:15 -0800

    Added aws static links.

--- awdavies.py
+++ awdavies.py
@@ -12,11 +12,17 @@ from projects import project_list
 from contact import contact_list
 site = Flask(__name__)
 
+# URLs that may or may not be important.
+urls = {
+	"static" : "https://s3.amazonaws.com/awdavies-static/",
+}
+
 @site.route("/")
 def main():
   return render_template("index.html",
           projects=project_list,
-          contact=contact_list)
+          contact=contact_list
+					urls=urls)
 
 if __name__ == "__main__":
     site.run()
