40431c99470540ec7bc85a39d3caa64690fa35f4
awdavies <a.w.davies.vio@gmail.com>
2017-02-14 15:48:01 -0800

    Woops on some links.

--- awdavies.py
+++ awdavies.py
@@ -14,15 +14,16 @@ site = Flask(__name__)
 
 # URLs that may or may not be important.
 urls = {
-	"static" : "https://s3.amazonaws.com/awdavies-static/",
+	"static" : "https://s3.amazonaws.com/awdavies-static",
 }
 
 @site.route("/")
 def main():
   return render_template("index.html",
           projects=project_list,
-          contact=contact_list
-					urls=urls)
+          contact=contact_list,
+					urls=urls,
+				 )
 
 if __name__ == "__main__":
     site.run()
